1a1c579fe84098e4a6a6612c0dfb765fee2905bd
phohtoo <phohtoo@users.noreply.github.com>
2023-10-08 11:36:49 +0000

    deploy: b3443ec2a456cd47b3ced313feac27443f7dece9

--- head/config_docs_generate_test.go
+++ head/config_docs_generate_test.go
@@ -27,20 +27,41 @@ import (
 
 	"github.com/hyperledger/firefly-common/pkg/config"
 	"github.com/hyperledger/firefly/internal/apiserver"
+	"github.com/hyperledger/firefly/internal/coreconfig"
 	"github.com/hyperledger/firefly/internal/namespace"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerateConfigDocs(t *testing.T) {
 	// Initialize config of all plugins
-	namespace.NewNamespaceManager(false)
+	coreconfig.Reset()
+	namespace.InitConfig()
 	apiserver.InitConfig()
 	f, err := os.Create(filepath.Join("reference", "config.md"))
 	assert.NoError(t, err)
-	generatedConfig, err := config.GenerateConfigMarkdown(context.Background(), config.GetKnownKeys())
+	generatedConfig, err := config.GenerateConfigMarkdown(context.Background(), configDocHeader, config.GetKnownKeys())
 	assert.NoError(t, err)
 	_, err = f.Write(generatedConfig)
 	assert.NoError(t, err)
 	err = f.Close()
 	assert.NoError(t, err)
 }
+
+const configDocHeader = `---
+layout: default
+title: Configuration Reference
+parent: pages.reference
+nav_order: 2
+---
+
+# Configuration Reference
+{: .no_toc }
+
+<!-- ## Table of contents
+{: .no_toc .text-delta }
+
+1. TOC
+{:toc} -->
+
+---
+`
